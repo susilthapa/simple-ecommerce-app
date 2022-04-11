1f8ef65c776906af38a4d7bcbeb2c565ea7d343b
susilthapa <susil.thapa@wiseyak.com>
2022-04-12 02:03:18 +0545

    Items filter bar added

---
+++ src/components/ItemsFilterBar.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import InputField from "./controls/InputField";
+
+import Select from "./controls/Select";
+
+function ItemsFilterBar() {
+  return (
+    <div className="flex  items-center justify-between w-full mt-10 2xl:mt-13">
+      <InputField
+        className="w-80"
+        placeholder="Apple Watch, samgsung S21, Mackbook Pro..."
+      />
+      <Select
+        placeholder="Categories"
+        className="w-60"
+        value="Apple"
+        options={["Apple", "Samgsung"]}
+      />
+    </div>
+  );
+}
+
+export default ItemsFilterBar;
