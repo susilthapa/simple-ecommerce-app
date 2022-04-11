f0c19ffc93d99d9cad2c4b32d55e7fee90a76f25
susilthapa <susil.thapa@wiseyak.com>
2022-04-12 02:03:35 +0545

    Initial layout setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Select from "./components/controls/Select";
+import ItemsFilterBar from "./components/ItemsFilterBar";
+
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <h1 className="text-3xl font-bold text-red-500">Hello world!</h1>;
+  return (
+    <div className="w-screen h-screen overflow-y-auto bg-background px-10 py-6 2xl:px-20 2xl:py-8">
+      <Navbar />
+      <ItemsFilterBar />
+    </div>
+  );
 }
 
 export default App;
