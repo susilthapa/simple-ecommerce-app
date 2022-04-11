a9f9cdaca9da88c6a6ab674dcb5bda37862d0449
susilthapa <susil.thapa@wiseyak.com>
2022-04-12 01:22:38 +0545

    Custom colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#d3d3d3",
+        primary: {
+          DEFAULT: "#f5f5f5",
+        },
+      },
+    },
   },
   plugins: [],
 };
