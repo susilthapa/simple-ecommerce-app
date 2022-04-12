85300ca27abf2f1cc7e1cf6cef7cac28956b93e6
susilthapa <susil.thapa@wiseyak.com>
2022-04-12 22:55:27 +0545

    Link added in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
-    <div className="sticky top-0 flex justify-between italic font-medium text-lg px-4 py-2.5 bg-white rounded-lg w-full  shadow-lg">
-      <h2>UPayments</h2>
+    <div className="sticky z-50 top-0 flex justify-between italic font-medium text-lg px-4 py-2.5 bg-white rounded-lg w-full  shadow-lg">
+      <Link to="/">
+        <h2 className="hover:scale-105 transition">UPayments</h2>
+      </Link>
       <a href="#" className="transition hover:scale-105">
         Register
       </a>
