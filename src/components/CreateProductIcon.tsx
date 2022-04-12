439114ea650f0f5181a0769739cecd1f04854227
susilthapa <susil.thapa@wiseyak.com>
2022-04-12 22:55:03 +0545

    Fixed positioned create product item

--- src/components/CreateProductIcon.tsx
+++ src/components/CreateProductIcon.tsx
@@ -5,7 +5,7 @@ import { PlusSmIcon } from "@heroicons/react/solid";
 
 function CreateProductIcon() {
   return (
-    <Link to="/create-product" className="absolute bottom-10 right-12">
+    <Link to="/create-product" className="fixed bottom-10 right-12">
       <PlusSmIcon className="h-10 w-10 text-white bg-black rounded-full hover:rotate-180 transition" />
     </Link>
   );
