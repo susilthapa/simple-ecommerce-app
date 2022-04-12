566873c30da0c01c2c9f44b0e18bc87f84706747
susilthapa <susil.thapa@wiseyak.com>
2022-04-12 22:46:52 +0545

    Route setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import React from "react";
-import Select from "./components/controls/Select";
-import ItemCard from "./components/ItemCard";
-import ItemList from "./components/ItemList";
-import ItemsFilterBar from "./components/ItemsFilterBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import ProductList from "./components/ProductList";
 
 import Navbar from "./components/Navbar";
+import CreateProductForm from "./components/CreateProductForm";
 
 function App() {
   return (
-    <div className="relative w-screen h-screen overflow-y-auto bg-background px-10 py-6 2xl:px-20 2xl:py-8">
-      <Navbar />
-      <ItemsFilterBar />
-      <ItemList />
-    </div>
+    <Router>
+      <div className="relative w-screen h-screen overflow-y-auto bg-background px-10 py-6 2xl:px-20 2xl:py-8">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/create-product" element={<CreateProductForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
