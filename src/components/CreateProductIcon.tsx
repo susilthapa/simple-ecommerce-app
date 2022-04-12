fe4bb7f150be4bc2d646417a749025df92e8b630
susilthapa <susil.thapa@wiseyak.com>
2022-04-12 22:27:41 +0545

    Create product icon

---
+++ src/components/CreateProductIcon.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+import { PlusSmIcon } from "@heroicons/react/solid";
+
+function CreateProductIcon() {
+  return (
+    <Link to="/create-product" className="absolute bottom-10 right-12">
+      <PlusSmIcon className="h-10 w-10 text-white bg-black rounded-full hover:rotate-180 transition" />
+    </Link>
+  );
+}
+
+export default CreateProductIcon;
