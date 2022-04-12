1c6fabf9a9a204421fbe637847fd93fb23667d2f
susilthapa <susil.thapa@wiseyak.com>
2022-04-13 00:51:00 +0545

    heading updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ function Navbar() {
   return (
     <div className="sticky z-50 top-0 flex justify-between italic font-medium text-lg px-4 py-2.5 bg-white rounded-lg w-full  shadow-lg">
       <Link to="/">
-        <h2 className="hover:scale-105 transition">UPayments</h2>
+        <h2 className="hover:scale-105 transition">UPayments Store</h2>
       </Link>
       <a href="#" className="transition hover:scale-105">
         Register
