037895c9c50fed951212017c09368bd6f5b3e6ad
susilthapa <susil.thapa@wiseyak.com>
2022-04-12 23:30:10 +0545

    Displaying list of products from api

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,17 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import WatchImg from "../assets/images/watch.jpeg";
+import { ProductType } from "./ProductList";
 
-function ProductCard() {
+interface Iprops {
+  product: ProductType;
+}
+function ProductCard({ product }: Iprops) {
+  const { name, price, id } = product;
   return (
     <div className="w-28">
-      <div className="w-full h-32 bg-white rounded-lg p-3 flex items-center">
-        <img src={WatchImg} alt="watch" className="w-full h-auto" />
-      </div>
+      <Link to="/">
+        <div className="group w-full h-32 bg-white rounded-lg p-3 flex items-center">
+          <img
+            src={WatchImg}
+            alt="watch"
+            className="w-full h-auto group-hover:scale-110 transition"
+          />
+        </div>
+      </Link>
       <div className="text-xs mt-2">
-        <p>Apple Watch</p>
+        <p>{name}</p>
         <div className="text-center">
-          <p>$500.6</p>
+          <p>${price}</p>
         </div>
       </div>
     </div>
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import CreateProductIcon from "./CreateProductIcon";
 import ProductCard from "./ProductCard";
 import ProductFilterBar from "./ProductFilterBar";
+import { getProducts } from "../shared/apiHelpers";
+import Spinner from "../shared/components/Spinner";
+
+export type ProductType = {
+  id: string;
+  name: string;
+  price: string;
+  description: string;
+  avatar: string;
+};
 
 function ProductList() {
+  const [products, setProducts] = useState<ProductType[]>();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      setIsLoading(true);
+      const { status, data } = await getProducts();
+      if (status) {
+        setProducts(data);
+      }
+      setIsLoading(false);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <div className="mt-10 2xl:mt-13 w-full">
       <ProductFilterBar />
       <div className="mt-12 w-11/12 md:w-1/2 m-auto flex flex-wrap gap-9">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {products?.map((product) => (
+          <ProductCard product={product} />
+        ))}
       </div>
       <CreateProductIcon />
+      {isLoading && <Spinner />}
     </div>
   );
 }
