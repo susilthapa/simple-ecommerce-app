5e2e91fb94d7cdba8c6609742de9a5551b7fedd7
susilthapa <susil.thapa@wiseyak.com>
2022-04-14 22:00:32 +0545

    Custom font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontSize: {
+        xxs: "0.7rem",
+      },
       colors: {
         background: "#d3d3d3",
         primary: {
